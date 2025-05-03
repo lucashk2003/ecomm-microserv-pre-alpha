b211938622e3a94cbd05b3b9196628f87875648a
lucashk2003 <104373202+lucashk2003@users.noreply.github.com>
2025-05-03 14:33:20 -0300

    Add files via upload

---
+++ app-commerce/app/services/mscatalogo_service.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from app.models import Producto
+from app.mapping import ProductoSchema
+producto_schema = ProductoSchema()
+
+class ClienteCatalogoService:
+
+    def __init__(self):
+        self.URL = os.getenv('MSCATALOGO_URL', 'http://localhost:5001/api/v1/')
+    
+    def obtener_producto(self, id: int) -> Producto:
+        result = None
+        r = requests.get(f'{self.URL}catalogo/productos/{id}')
+        if r.status_code == 200:
+            result = producto_schema.load(r.json())
+        else:
+            raise BaseException("Error en el servicio 1")
+        return result
